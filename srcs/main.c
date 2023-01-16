9298d69e44190462cae9c78ed8d7689ede3252e5
Stephen ELIE <stelie@student.42.fr>
2023-01-16 18:32:12 +0100

    removed useless testing things

--- srcs/main.c
+++ srcs/main.c
@@ -6,34 +6,13 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:03:38 by dcyprien          #+#    #+#             */
-/*   Updated: 2023/01/16 16:38:54 by stelie           ###   ########.fr       */
+/*   Updated: 2023/01/16 18:26:32 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-
-typedef enum e_direction
-{
-	N = 2,
-	S,
-	W,
-	E
-}			t_direction;
-
-int	worldMap[7][7] = {
-	{1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, N, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 1},
-	{1, 1, 1, 1, 1, 1, 1},
-};
-
-static int	_exit_routine(void *mlx)
+/* static int	_exit_routine(void *mlx)
 {
 	if (mlx == NULL)
 		return (EXIT_FAILURE);
@@ -46,7 +25,7 @@ static int	_exit_routine(void *mlx)
 static int	_routine(void	*mlx)
 {
 	return (_exit_routine(mlx));
-}
+} */
 
 /**
  * TODO: ERR_MSG if (ac != 2)
@@ -67,6 +46,7 @@ int	main(int ac, char **av)
 	printf("\ncolors\n");
 	printf("floor = %d,%d,%d\n", ptr->floor[0], ptr->floor[1],ptr->floor[2]);
 	printf("ceili = %d,%d,%d\n", ptr->ceiling[0], ptr->ceiling[1],ptr->ceiling[2]);
+	init_mlx(ptr);
 	free_them_all(ptr);
-	return (_routine(NULL));
+	return (EXIT_SUCCESS);
 }
