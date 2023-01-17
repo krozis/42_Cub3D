ddaaef663baca6357a6167ce7d5918b681c25d4c
Stephen ELIE <stelie@student.42.fr>
2023-01-17 12:17:12 +0100

    corrected ptr->x_text + added free_mlx()

--- srcs/main.c
+++ srcs/main.c
@@ -6,30 +6,12 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:03:38 by dcyprien          #+#    #+#             */
-/*   Updated: 2023/01/17 11:34:09 by stelie           ###   ########.fr       */
+/*   Updated: 2023/01/17 12:15:02 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-/* static int	_exit_routine(void *mlx)
-{
-	if (mlx == NULL)
-		return (EXIT_FAILURE);
-	write(1, "YO", 2);
-	mlx_destroy_display(mlx);
-	free(mlx);
-	return (EXIT_SUCCESS);
-}
-
-static int	_routine(void	*mlx)
-{
-	return (_exit_routine(mlx));
-} */
-
-/**
- * TODO: ERR_MSG if (ac != 2)
- */
 int	main(int ac, char **av)
 {
 	t_ptr	*ptr;
@@ -38,10 +20,10 @@ int	main(int ac, char **av)
 		return (0);
 	ptr = init(av);
 	init_mlx(ptr);
-	printf("\nnorth texture = %s\n", ptr->n_text);
-	printf("south texture = %s\n", ptr->s_text);
-	printf("east  texture = %s\n", ptr->e_text);
-	printf("west  texture = %s\n", ptr->w_text);
+	printf("\nnorth texture = %s\n", ptr->text[N_TEXT]);
+	printf("south texture = %s\n", ptr->text[S_TEXT]);
+	printf("east  texture = %s\n", ptr->text[E_TEXT]);
+	printf("west  texture = %s\n", ptr->text[W_TEXT]);
 	if (check_errors(ptr) == EXIT_FAILURE)
 		return (0);
 	printf("test map\n");
@@ -50,6 +32,7 @@ int	main(int ac, char **av)
 	printf("\ncolors\n");
 	printf("floor = %d,%d,%d\n", ptr->floor[0], ptr->floor[1],ptr->floor[2]);
 	printf("ceili = %d,%d,%d\n", ptr->ceiling[0], ptr->ceiling[1],ptr->ceiling[2]);
+	free_mlx(ptr, true, true, EXIT_SUCCESS);
 	free_them_all(ptr);
 	return (EXIT_SUCCESS);
 }
