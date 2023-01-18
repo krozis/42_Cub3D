ad105657cacc0c396671c6182006942f41c6d95f
Stephen ELIE <stelie@student.42.fr>
2023-01-18 12:11:36 +0100

    remade colors getter

--- srcs/colors.c
+++ srcs/colors.c
@@ -3,72 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   colors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dcyprien <dcyprien@student.42.fr>          +#+  +:+       +#+        */
+/*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 15:26:04 by dcyprien          #+#    #+#             */
-/*   Updated: 2023/01/17 17:36:49 by dcyprien         ###   ########.fr       */
+/*   Updated: 2023/01/18 12:09:47 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	init_color(t_ptr *ptr)
+static int	convert_colors(int r, int g, int b)
 {
-	ptr->floor[0] = -1;
-	ptr->floor[1] = -1;
-	ptr->floor[2] = -1;
-	ptr->ceiling[0] = -1;
-	ptr->ceiling[1] = -1;
-	ptr->ceiling[2] = -1;
+	int	color;
+
+	color = (r * RGB_RED) + (g * RGB_GREEN) + (b * RGB_BLUE);
+	return (color);
 }
 
-void	ceiling_color(char *str, t_ptr *ptr, int i)
+static void	ceiling_color(char *str, t_ptr *ptr, int i)
 {
+	int	ceiling[3];
+
 	i++;
 	while (str[i] && ft_isspace(str[i]))
 		i++;
 	if (!ft_isdigit(str[i]))
-		ptr->ceiling[0] = -1;
+		ceiling[0] = -1;
 	else
-		ptr->ceiling[0] = ft_atoi(&str[i]);
+		ceiling[0] = ft_atoi(&str[i]);
 	while (str[i++] != ',')
 		;
 	if (!ft_isdigit(str[i]))
-		ptr->ceiling[1] = -1;
+		ceiling[1] = -1;
 	else
-		ptr->ceiling[1] = ft_atoi(&str[i]);
+		ceiling[1] = ft_atoi(&str[i]);
 	while (str[i++] != ',')
 		;
 	if (!ft_isdigit(str[i]))
-		ptr->ceiling[2] = -1;
+		ceiling[2] = -1;
 	else
-		ptr->ceiling[2] = ft_atoi(&str[i]);
+		ceiling[2] = ft_atoi(&str[i]);
+	if (check_colors(ceiling) == EXIT_SUCCESS)
+		ptr->ceiling_color = convert_colors(ceiling[0], ceiling[1], ceiling[2]);
 }
 
-void	floor_color(char *str, t_ptr *ptr, int i)
+static void	floor_color(char *str, t_ptr *ptr, int i)
 {
+	int	floor[3];
+
 	i++;
 	while (str[i] && ft_isspace(str[i]))
 		i++;
 	if (!ft_isdigit(str[i]))
-		ptr->floor[0] = -1;
+		floor[0] = -1;
 	else
-		ptr->floor[0] = ft_atoi(&str[i]);
+		floor[0] = ft_atoi(&str[i]);
 	while (str[i++] != ',')
 		;
 	if (!ft_isdigit(str[i]))
-		ptr->floor[1] = -1;
+		floor[1] = -1;
 	else
-		ptr->floor[1] = ft_atoi(&str[i]);
+		floor[1] = ft_atoi(&str[i]);
 	while (str[i++] != ',')
 		;
 	if (!ft_isdigit(str[i]))
-		ptr->floor[2] = -1;
+		floor[2] = -1;
 	else
-		ptr->floor[2] = ft_atoi(&str[i]);
+		floor[2] = ft_atoi(&str[i]);
+	if (check_colors(floor) == EXIT_SUCCESS)
+		ptr->floor_color = convert_colors(floor[0], floor[1], floor[2]);
 }
 
-void	set_colors(char *str, t_ptr *ptr)
+static void	set_colors(char *str, t_ptr *ptr)
 {
 	int	i;
 
@@ -79,16 +85,24 @@ void	set_colors(char *str, t_ptr *ptr)
 		ceiling_color(str, ptr, i);
 	if (str[i] == 'F')
 		floor_color(str, ptr, i);
-	convert_colors(ptr);
 }
 
+/**
+ * TODO: if multiple F/C lines, the last one is used -> force only one?
+ * TODO: can't read F/C line with spaces into the rgb code (ex: 25, 1, 50)
+ * @brief Get colors in the lines starting by F or C
+ * 
+ * @param lines the lines that have been read in the map file
+ * @param ptr the main structure
+ */
 void	get_colors(char **lines, t_ptr *ptr)
 {
 	int	i;
 	int	k;
 
 	i = 0;
-	init_color(ptr);
+	ptr->floor_color = -1;
+	ptr->ceiling_color = -1;
 	if (!lines)
 		return ;
 	while (lines[i])
