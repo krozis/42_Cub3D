063a17ca9921a9ad8a745c7be62d600b361be3a2
Stephen ELIE <stelie@student.42.fr>
2023-02-06 17:52:50 +0100

    corrected still moving mouse when SPACE key is pressed

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -6,7 +6,7 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 14:00:43 by stelie            #+#    #+#             */
-/*   Updated: 2023/02/06 17:31:37 by stelie           ###   ########.fr       */
+/*   Updated: 2023/02/06 17:52:00 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,8 @@ void	rotate(t_c3d *c3d)
 
 	olddirx = c3d->player->dir_x;
 	oldplanex = c3d->player->plane_x;
-	if (c3d->keys.rotate_left || c3d->keys.rotate_mouse_left)
+	if (c3d->keys.rotate_left
+		|| (c3d->keys.rotate_mouse_left && c3d->keys.mouse_free == false))
 	{
 		c3d->player->dir_x
 			= rot_l(c3d->player->dir_x, c3d->player->dir_y, true);
@@ -46,7 +47,8 @@ void	rotate(t_c3d *c3d)
 			= rot_l(c3d->player->plane_x, c3d->player->plane_y, true);
 		c3d->player->plane_y = rot_l(oldplanex, c3d->player->plane_y, false);
 	}
-	if (c3d->keys.rotate_right || c3d->keys.rotate_mouse_right)
+	if (c3d->keys.rotate_right
+		|| (c3d->keys.rotate_mouse_right && c3d->keys.mouse_free == false))
 	{
 		c3d->player->dir_x
 			= rot_r(c3d->player->dir_x, c3d->player->dir_y, true);
